ed46397f8fb5d74fe95fb707b5dac45740b58323
Jessy Sztark <Jessy.Sztark@etu.univ-savoie.fr>
2023-03-06 17:30:34 +0100

    [1.0.3] Account, Address and Country classes in CodeFirst

---
+++ Miliboo/Models/EntityFramework/Country.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Miliboo.Models.EntityFramework
+{
+    [Table("T_E_COUNTRY_CNT")]
+    public class Country
+    {
+        private int cnt_id;
+        private String? cnt_wording;
+        private String? cnt_phonecode;
+
+        public Country() {
+
+        }
+
+        [Key]
+        [Column("cnt_id")]
+        public int CountryID
+        {
+            get { return cnt_id; }
+            set { cnt_id = value; }
+        }
+
+        [Column("cnt_wording", TypeName="varchar")]
+        [MaxLength(100)]
+        public String? Wording
+        {
+            get { return cnt_wording; }
+            set { cnt_wording = value; }
+        }
+
+        [Column("cnt_phonecode", TypeName = "varchar")]
+        [MaxLength(4)]
+        public String? PhoneCode
+        {
+            get { return cnt_phonecode; }
+            set { cnt_phonecode = value; }
+        }
+    }
+}
---
+++ Miliboo/Models/EntityFramework/MilibooDBContext.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Miliboo.Models.EntityFramework {
+    public class MilibooDBContext : DbContext {
+        public static readonly ILoggerFactory Mylogs = LoggerFactory.Create(builder => builder.AddConsole());
+
+        public MilibooDBContext() { }
+
+        public MilibooDBContext(DbContextOptions<MilibooDBContext> options)
+        : base(options) {
+        }
+
+        public virtual DbSet<Account> ACT { get; set; }
+        public virtual DbSet<Address> ADR { get; set; }
+        public virtual DbSet<Country> CNT { get; set; }
+
+        protected void OnModelCreating(ModelBuilder modelBuilder) {
+            modelBuilder.Entity<Account>(entity => {
+
+                entity.HasIndex(a => a.Mail)
+                .IsUnique();
+            });
+        }
+
+    }
+}
