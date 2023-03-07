853564762ca7647e5781a677af49fa16e3cb279c
Adrien Chenu <Adrien.Chenu@etu.univ-smb.fr>
2023-03-07 10:13:27 +0100

    A

---
+++ Miliboo/Models/EntityFramework/Grouping.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Miliboo.Models.EntityFramework;
+
+[Table("t_e_grouping_grp")]
+
+public partial class Grouping
+{
+    private int groupingId;
+    private string? groupingName;
+
+    [Key]
+    [Column("grp_id")]
+    public int GroupingId { get; set; }
+
+    [Column("grp_groupingName", TypeName = "varchar(50)")]
+    public string? GroupingName { get; set; }
+
+
+    [InverseProperty("GroupingsNavigation")]
+    public virtual ICollection<Grouping> GroupingsRegroup { get; set; } = new List<Grouping>();
+}
