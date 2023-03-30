300a30ff0fd7b218c6caed74beb06d4248be2654
Jessy Sztark <61654832+JessySztark@users.noreply.github.com>
2023-03-30 14:15:30 +0200

    Update User.cs for JWT Token

--- Miliboo/Models/User.cs
+++ Miliboo/Models/User.cs
@@ -1,11 +1,9 @@
-﻿namespace Miliboo.Models
+namespace Miliboo.Models
 {
     public class User
     {
-        public string UserName { get; set; }
-        public string? FullName { get; set; }
+        public string Mail { get; set; }
         public string Password { get; set; }
         public string? UserRole { get; set; }
-
     }
 }
